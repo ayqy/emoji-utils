b2cd8c76b1acb3ec99d6809d39cd12da7498b239
anglezhang <anglezhang@tencent.com>
2021-07-21 23:21:13 +0800

    remove code for 2b1b & 2b1c

--- emoutils.js
+++ emoutils.js
@@ -15,7 +15,6 @@ const EMOJIS = [
 ];
 // zero width joiner
 const EMOJI_JOINER = /\u200d/;
-const EMOJI_JOIN_SQUARE = /\u2b1b|\u2b1c/;
 // 后缀控制符
 const EMOJI_INDICATORS = [
   // VARIATION SELECTOR-15 (VS15), used to request a text presentation for an emoji character.
@@ -44,15 +43,7 @@ function _matchJoined(str) {
     // 吃掉连接符
     matched += matchedJoiner[0];
     str = str.substr(matchedJoiner[0].length);
-    // join Colored squares \u2b1b \u2b1c
-    const squareJoinMatched = str[0].match(EMOJI_JOIN_SQUARE);
-    if (squareJoinMatched) {
-      matched += squareJoinMatched[0];
-      str = str.substr(squareJoinMatched[0].length);
-      matched += _matchJoined(str);
-    } else {
-      matched += matchOneEmoji(str);
-    }
+    matched += matchOneEmoji(str);
   }
 
   return matched;
